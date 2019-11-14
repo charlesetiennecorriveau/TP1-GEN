041a602f2f587d7315f58fcede9c64910c3f9991
jeremy <jeremy@xydmz.net>
2019-11-13 23:06:28 -0500

    Added film details api

--- TP1-GEN/TP1-GEN/Models/APICaller.cs
+++ TP1-GEN/TP1-GEN/Models/APICaller.cs
@@ -9,10 +9,12 @@ namespace TP1_GEN.Models
 {
     public class APICaller
     {
+        const string ApiKey = "42ea10e4b84b36c36a064a410b108b2f";
+
         public static async Task<List<FilmOverview>> GetNowPlayingAsync(int page)
         {
             // This got our key in it OwO
-            string apiUrl = "https://api.themoviedb.org/3/movie/now_playing?api_key=42ea10e4b84b36c36a064a410b108b2f&page=" + page;
+            string apiUrl = $"https://api.themoviedb.org/3/movie/now_playing?api_key={ApiKey}&page={page}";
 
             List<FilmOverview> filmsNowPlaying = new List<FilmOverview>();
 
@@ -27,7 +29,6 @@ namespace TP1_GEN.Models
                 JObject jObject = JObject.Parse(data);
 
                 
-
                 foreach (var film in jObject["results"])
                 {
                     filmsNowPlaying.Add(new FilmOverview
@@ -37,11 +38,42 @@ namespace TP1_GEN.Models
                         Id = (int)film["id"]
                     }); ;
                 }
-
-
             }
-
             return filmsNowPlaying;
+        }
+
+        public static async Task<Film> getFilmDetailsAsync(int id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string apiUrl = $"https://api.themoviedb.org/3/movie/{id}?api_key={ApiKey}";
+
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+
+                client.BaseAddress = new Uri(apiUrl);
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                var data = await response.Content.ReadAsStringAsync();
+                JObject filmResult = JObject.Parse(data);
+
+
+                //apiUrl = $"https://api.themoviedb.org/3/movie/{id}?api_key={ApiKey}";
+
+                //client.BaseAddress = new Uri(apiUrl);
+                //response = await client.GetAsync(apiUrl);
+                //data = await response.Content.ReadAsStringAsync();
+
+                Film film = new Film
+                {
+                    PosterUrl = (string)filmResult["poster_path"],
+                    Rating = (int)filmResult["vote_average"],
+                    Summary = (string)filmResult["overview"],
+                    Title = (string)filmResult["title"]
+                };
+
+                return film;
+            }
         }
     }
 }
