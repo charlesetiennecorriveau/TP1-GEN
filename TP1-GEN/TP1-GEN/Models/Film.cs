c04a2c3f5691d5d9fe449141266ff94c465b1ada
jeremy <jeremy@xydmz.net>
2019-11-13 20:34:53 -0500

    Added basic model

---
+++ TP1-GEN/TP1-GEN/Models/Film.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TP1_GEN.Models
+{
+    public class Film
+    {
+        public string PosterUrl { get; }
+        public string Title { get; }
+        public string Summary { get; }
+        public List<string> Casting { get; }
+        public List<string> Team { get; }
+        public float Rating { get; }
+    }
+}
